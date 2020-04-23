cdce265c9019b013b3f9966489a26cd552b8eaf6
natewagner10 <54585457+natewagner10@users.noreply.github.com>
2020-04-23 14:45:16 -0400

    Update sentiment_intensity.py

--- sentiment_intensity.py
+++ sentiment_intensity.py
@@ -24,11 +24,24 @@ total_sent = target.map(posOrneg)
 def filter_rdd(line):
     return line[4], line[18], line[20], line[21], line[22], line[23], line[24]
 
-total_sent_df = total_sent.map(filter_rdd).toDF().selectExpr("_1 as message", "_2 as paid_for_by", "_3 as neg", "_4 as neu", "_5 as pos", "_6 as sentiment", "_7 as score")
+def createSome(line):
+    if line[5] == "neutral":
+        if line[2] >= 0.15:
+            return line[0], line[1], line[2], line[3], line[4], "somewhat negative", line[6]
+        if line[4] >= 0.15:
+            return line[0], line[1], line[2], line[3], line[4], "somewhat positive", line[6]
+        else:
+            return line[0], line[1], line[2], line[3], line[4], line[5], line[6]
+    else:
+        return line[0], line[1], line[2], line[3], line[4], line[5], line[6]
+        
+
+
+total_sent_df = total_sent.map(filter_rdd).map(createSome).toDF().selectExpr("_1 as message", "_2 as paid_for_by", "_3 as neg", "_4 as neu", "_5 as pos", "_6 as sentiment", "_7 as score")
 total_sent_df.show(5)
 total_sent_df.createOrReplaceTempView("sent")
 
-
+query = sqlContext.sql("select sentiment, count(sentiment) cnt from sent group by sentiment")
 query = sqlContext.sql("select paid_for_by, sentiment, count(sentiment) cnt from sent group by paid_for_by, sentiment")
 #query.toPandas().to_csv('total_sentiment.csv')
 

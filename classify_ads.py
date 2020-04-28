83fdf9cda758f25094ac1d0ae45fcab5346e4aba
natewagner10 <54585457+natewagner10@users.noreply.github.com>
2020-04-28 19:52:41 -0400

    Update classify_ads.py

--- classify_ads.py
+++ classify_ads.py
@@ -38,13 +38,26 @@ econ_wb = ['globalization', 'international relations', 'trade', 'finance',
            'unemployment rate', 'hires', 'job creation', 'wage', 'minimum wage',
            'paid sick leave', 'corporate tax', 'capital gains', 'capital gains tax',
            'compensation', 'labor costs', 'wages', 'wage data', 'union', 'nonunion', 'accounting',
-           'Business Administration', 'business Economics', 'marketing']
+           'Business Administration', 'business Economics', 'marketing',  'capitalism', 'socialism', 
+           'free market', 'real estate', 'real estate markets', 'mass transit', 'transit', 
+           'transportation', 'universal basic income', 'overtime pay', 'economic stimulus', 
+           'federal reserve', 'domestic jobs', 'tariffs', 'tariff', 'property taxes']
+
+envior_wb = ['climate change', 'fracking', 'alternative energy', 'oil drilling', 'environmental', 
+           'environment', 'EPA', 'epa', 'environmental protection agency', 'pollution', 'global warming', 
+           'environmental management system', 'sustainability', 'air pollution', 'energy', 
+           'overpopulation', 'deforestation', 'waste',  'environmental degradation', 'waste management', 
+           'water pollution', 'carbon footprint', 'biodiversity', 'genetic modification', 'ozone layer', 
+           'ozone layer depletion', 'mining', 'natural resource depletion', 'natural resources', 
+           'natural resource', 'radioactive', 'radioactive waste', 'nuclear energy', 'acid rain', 'endangered', 
+           'endangered species', 'light pollution', 'noise pollution', 'urban sprawl', 'litter', 'littering', 'landfill']
 
 def classify_immigrant(line):
     try:
         cnt_imm = 0
         cnt_health = 0
         cnt_econ = 0
+        cnt_envior = 0
         for word in line[5]:
             if word in imm_wb:
                 cnt_imm += 1
@@ -52,10 +65,12 @@ def classify_immigrant(line):
                 cnt_health += 1
             if word in econ_wb:
                 cnt_econ += 1
+            if word in envior_wb:
+                cnt_envior += 1
         # list of the number of occurrences 
-        counts = [cnt_imm, cnt_health, cnt_econ]
+        counts = [cnt_imm, cnt_health, cnt_econ, cnt_envior]
         # list of the classes
-        class_names = ["immigration", "healthcare", "economic"] 
+        class_names = ["immigration", "healthcare", "economic", "environment"] 
         # get the index location of the max element
         da_max = counts.index(max(counts))
         # testing if any of the counts equal each other, or all are zero

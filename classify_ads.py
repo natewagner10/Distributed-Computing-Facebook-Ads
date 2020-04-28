9cf6dbeb60e056003c504e61adb8d56d455ea492
natewagner10 <54585457+natewagner10@users.noreply.github.com>
2020-04-28 17:10:05 -0400

    Update classify_ads.py

--- classify_ads.py
+++ classify_ads.py
@@ -26,11 +26,14 @@ imm_wb = ["immigration", "muslim", "border", "wall", "entering the country", "al
 
 def classify_immigrant(line):
     try:
-      for word in line[5]:
-          if word in imm_wb:
-              return line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[7], line[8], line[9], line[10], line[11], line[12], line[13], line[14], line[15], line[16], line[17], line[18], line[19], line[20], 'immigration'
-          else:
-              return line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[7], line[8], line[9], line[10], line[11], line[12], line[13], line[14], line[15], line[16], line[17], line[18], line[19], line[20], 'other'
+        cnt = 0
+        for word in line[5]:
+            if word in imm_wb:
+                cnt += 1           
+        if cnt != 0:
+            return line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[7], line[8], line[9], line[10], line[11], line[12], line[13], line[14], line[15], line[16], line[17], line[18], line[19], line[20], 'immigration'
+        else:
+            return line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[7], line[8], line[9], line[10], line[11], line[12], line[13], line[14], line[15], line[16], line[17], line[18], line[19], line[20], 'other'
     except:
         return line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[7], line[8], line[9], line[10], line[11], line[12], line[13], line[14], line[15], line[16], line[17], line[18], line[19], line[20], 'other'
 

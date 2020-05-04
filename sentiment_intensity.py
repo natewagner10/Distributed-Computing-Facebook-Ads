bbfb74c59d5114abcd0f40bdc36db3b43757f678
natewagner10 <54585457+natewagner10@users.noreply.github.com>
2020-05-04 09:53:24 -0400

    Update sentiment_intensity.py

--- sentiment_intensity.py
+++ sentiment_intensity.py
@@ -9,36 +9,41 @@ def posOrneg(line):
     neu = sentiment['neu']
     pos = sentiment['pos']
     if neg > neu and neg > pos:
-        return line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[7], line[8], line[9], line[10], line[11], line[12], line[13], line[14], line[15], line[16], line[17], line[18], line[19], neg, neu, pos, "negative", sentiment['compound']
+        return line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[7], line[8], line[9], line[10], line[11], line[12], line[13], line[14], line[15], line[16], line[17], line[18], line[19], line[20], line[21], neg, neu, pos, "negative", sentiment['compound']
     if neu > neg and neu > pos:
-        return line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[7], line[8], line[9], line[10], line[11], line[12], line[13], line[14], line[15], line[16], line[17], line[18], line[19], neg, neu, pos, "neutral", sentiment['compound']
+        return line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[7], line[8], line[9], line[10], line[11], line[12], line[13], line[14], line[15], line[16], line[17], line[18], line[19], line[20], line[21],  neg, neu, pos, "neutral", sentiment['compound']
     if pos > neg and pos > neu:
-        return line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[7], line[8], line[9], line[10], line[11], line[12], line[13], line[14], line[15], line[16], line[17], line[18], line[19], neg, neu, pos, "positive", sentiment['compound']
+        return line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[7], line[8], line[9], line[10], line[11], line[12], line[13], line[14], line[15], line[16], line[17], line[18], line[19], line[20], line[21],  neg, neu, pos, "positive", sentiment['compound']
     else:
-        return line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[7], line[8], line[9], line[10], line[11], line[12], line[13], line[14], line[15], line[16], line[17], line[18], line[19], neg, neu, pos, "neutral", sentiment['compound']
+        return line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[7], line[8], line[9], line[10], line[11], line[12], line[13], line[14], line[15], line[16], line[17], line[18], line[19], line[20], line[21],  neg, neu, pos, "neutral", sentiment['compound']
     
 
 
-total_sent = target.map(posOrneg)
+total_sent = data_w_imm.map(posOrneg)
 
 def filter_rdd(line):
-    return line[4], line[18], line[20], line[21], line[22], line[23], line[24]
+    return line[4], line[19], line[21], line[22], line[23], line[24], line[25], line[7].month, line[7].day, line[7].year
 
 def createSome(line):
-    if line[5] == "neutral":
-        if line[2] >= 0.15:
-            return line[0], line[1], line[2], line[3], line[4], "somewhat negative", line[6]
-        if line[4] >= 0.15:
-            return line[0], line[1], line[2], line[3], line[4], "somewhat positive", line[6]
+    if line[6] == "neutral":
+        if line[3] >= 0.15:
+            return line[0], line[1], line[2], line[3], line[4], line[5], "somewhat negative", line[7], line[8], line[9]
+        if line[5] >= 0.15:
+            return line[0], line[1], line[2], line[3], line[4], line[5], "somewhat positive", line[7], line[8], line[9]
         else:
-            return line[0], line[1], line[2], line[3], line[4], line[5], line[6]
+            return line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[7], line[8], line[9]
     else:
-        return line[0], line[1], line[2], line[3], line[4], line[5], line[6]
+        return line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[7], line[8], line[9]
         
 
 
-total_sent_df = total_sent.map(filter_rdd).map(createSome).toDF().selectExpr("_1 as message", "_2 as paid_for_by", "_3 as neg", "_4 as neu", "_5 as pos", "_6 as sentiment", "_7 as score")
-total_sent_df.show(5)
+total_sent_feed = total_sent.map(filter_rdd).map(createSome)
+
+reduced_data = total_sent_feed.filter(lambda x: x[2] != 'other')
+      
+        
+reduced_data_df = reduced_data.toDF().selectExpr("_1 as message", "_2 as paid_for_by", "_3 as category", "_4 as neg", "_5 as neut", "_6 as pos", "_7 as sentiment", "_8 as month", "_9 as day", "_10 as year")
+reduced_data_df.show(10)
 total_sent_df.createOrReplaceTempView("sent")
 
 query = sqlContext.sql("select sentiment, count(sentiment) cnt from sent group by sentiment")
